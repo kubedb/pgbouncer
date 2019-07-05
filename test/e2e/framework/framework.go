513a58eb077563c014e84fcfa8d2e5a88f62cf79
Tamal Saha <tamal@appscode.com>
2019-07-05 00:43:58 -0700

    Clone kubedb/postgres repo
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ test/e2e/framework/framework.go
@@ -0,0 +1,88 @@
+package framework
+
+import (
+	"path/filepath"
+
+	"github.com/appscode/go/crypto/rand"
+	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
+	cs "github.com/kubedb/apimachinery/client/clientset/versioned"
+	. "github.com/onsi/gomega"
+	"github.com/spf13/afero"
+	"gomodules.xyz/cert/certstore"
+	crd_cs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1beta1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	ka "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset"
+	appcat_cs "kmodules.xyz/custom-resources/client/clientset/versioned/typed/appcatalog/v1alpha1"
+	scs "stash.appscode.dev/stash/client/clientset/versioned"
+)
+
+var (
+	DockerRegistry     = "kubedbci"
+	SelfHostedOperator = false
+	DBCatalogName      = "10.2-v4"
+)
+
+type Framework struct {
+	restConfig       *rest.Config
+	kubeClient       kubernetes.Interface
+	apiExtKubeClient crd_cs.ApiextensionsV1beta1Interface
+	dbClient         cs.Interface
+	kaClient         ka.Interface
+	appCatalogClient appcat_cs.AppcatalogV1alpha1Interface
+	stashClient      scs.Interface
+	namespace        string
+	name             string
+	StorageClass     string
+	CertStore        *certstore.CertStore
+}
+
+func New(
+	restConfig *rest.Config,
+	kubeClient kubernetes.Interface,
+	apiExtKubeClient crd_cs.ApiextensionsV1beta1Interface,
+	dbClient cs.Interface,
+	kaClient ka.Interface,
+	appCatalogClient appcat_cs.AppcatalogV1alpha1Interface,
+	stashClient scs.Interface,
+	storageClass string,
+) *Framework {
+	store, err := certstore.NewCertStore(afero.NewMemMapFs(), filepath.Join("", "pki"))
+	Expect(err).NotTo(HaveOccurred())
+
+	err = store.InitCA()
+	Expect(err).NotTo(HaveOccurred())
+	return &Framework{
+		restConfig:       restConfig,
+		kubeClient:       kubeClient,
+		apiExtKubeClient: apiExtKubeClient,
+		dbClient:         dbClient,
+		kaClient:         kaClient,
+		appCatalogClient: appCatalogClient,
+		stashClient:      stashClient,
+		name:             "postgres-operator",
+		namespace:        rand.WithUniqSuffix(api.ResourceSingularPostgres),
+		StorageClass:     storageClass,
+		CertStore:        store,
+	}
+}
+
+func (f *Framework) Invoke() *Invocation {
+	return &Invocation{
+		Framework: f,
+		app:       rand.WithUniqSuffix("postgres-e2e"),
+	}
+}
+
+func (fi *Invocation) App() string {
+	return fi.app
+}
+
+func (fi *Invocation) ExtClient() cs.Interface {
+	return fi.dbClient
+}
+
+type Invocation struct {
+	*Framework
+	app string
+}
