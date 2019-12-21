bb68095e3abba06b36b000ae5441482cb30fa012
Tamal Saha <tamal@appscode.com>
2019-12-20 22:16:00 -0800

    Update client-go to kubernetes-1.16.3
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/cmds/server/options.go
+++ pkg/cmds/server/options.go
@@ -37,7 +37,6 @@ import (
 )
 
 type ExtraOptions struct {
-	EnableRBAC                  bool
 	OperatorNamespace           string
 	RestrictToOperatorNamespace bool
 	GoverningService            string
@@ -60,7 +59,6 @@ func (s ExtraOptions) WatchNamespace() string {
 
 func NewExtraOptions() *ExtraOptions {
 	return &ExtraOptions{
-		EnableRBAC:        true,
 		OperatorNamespace: meta.Namespace(),
 		GoverningService:  "kubedb",
 		ResyncPeriod:      10 * time.Minute,
@@ -76,7 +74,6 @@ func NewExtraOptions() *ExtraOptions {
 
 func (s *ExtraOptions) AddGoFlags(fs *flag.FlagSet) {
 	fs.StringVar(&s.GoverningService, "governing-service", s.GoverningService, "Governing service for database statefulset")
-	fs.BoolVar(&s.EnableRBAC, "rbac", s.EnableRBAC, "Enable RBAC for operator & offshoot Kubernetes objects")
 
 	fs.Float64Var(&s.QPS, "qps", s.QPS, "The maximum QPS to the master from this client")
 	fs.IntVar(&s.Burst, "burst", s.Burst, "The maximum burst for throttle")
@@ -97,7 +94,6 @@ func (s *ExtraOptions) AddFlags(fs *pflag.FlagSet) {
 func (s *ExtraOptions) ApplyTo(cfg *controller.OperatorConfig) error {
 	var err error
 
-	cfg.EnableRBAC = s.EnableRBAC
 	cfg.OperatorNamespace = s.OperatorNamespace
 	cfg.GoverningService = s.GoverningService
 
