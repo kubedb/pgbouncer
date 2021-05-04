11c2ac03f14c7dc964e2a9fd364e21c138b79579
lgtm <1gtm@users.noreply.github.com>
2021-05-03 17:55:26 -0700

    Update Kubernetes toolchain to v1.21.0 (#155)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- pkg/cmds/server/start.go
+++ pkg/cmds/server/start.go
@@ -29,7 +29,6 @@ import (
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	genericoptions "k8s.io/apiserver/pkg/server/options"
-	"kmodules.xyz/client-go/meta"
 	"kmodules.xyz/client-go/tools/clientcmd"
 )
 
@@ -49,7 +48,6 @@ func NewPgBouncerServerOptions(out, errOut io.Writer) *PgBouncerServerOptions {
 		RecommendedOptions: genericoptions.NewRecommendedOptions(
 			defaultEtcdPathPrefix,
 			server.Codecs.LegacyCodec(admissionv1beta1.SchemeGroupVersion),
-			genericoptions.NewProcessInfo("pgbouncer-operator", meta.Namespace()),
 		),
 		ExtraOptions: NewExtraOptions(),
 		StdOut:       out,
